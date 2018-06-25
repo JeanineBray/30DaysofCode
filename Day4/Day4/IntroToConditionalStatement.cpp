fdd9ca32e6f1ac6c32c1c8c3914bedd0e7bdb7cf
Jeanine Bray <jeaninebray@outlook.com>
2018-06-25 15:18:09 -0400

    Day 4 Challenge

---
+++ Day4/Day4/IntroToConditionalStatement.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+#include <iomanip>
+#include <string>
+#include <iostream>
+#include <limits>
+
+//using namespace std;
+
+
+
+int main()
+{
+	int N;
+	std::cin >> N;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	if (N)
+	{
+
+	}
+
+	return 0;
+}
