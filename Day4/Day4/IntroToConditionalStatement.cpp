d0c193c7648130ec71a6963bdc77e007216b9666
Jeanine Bray <jeaninebray@outlook.com>
2018-06-25 15:50:14 -0400

    Update

--- Day4/Day4/IntroToConditionalStatement.cpp
+++ Day4/Day4/IntroToConditionalStatement.cpp
@@ -10,14 +10,38 @@
 
 int main()
 {
-	int N;
-	std::cin >> N;
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	//Declare data type and variable
+		int N;
 
-	if (N)
-	{
+	//Receive input for declared variable
+		std::cin >> N;
 
-	}
+	//Rinse buffer
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-	return 0;
+	//Conditional statement
+		if ((1 <= N) && (N <= 100))
+			{
+				if (N % 2 != 0)
+				{
+					std::cout << "Weird" << std::endl;
+				}
+				else if ((N % 2 == 0) && (2 <= N) && (N <= 5))
+				{
+					std::cout << "Not Weird" << std::endl;
+				}
+				else if ((N % 2 == 0) && (6 <= N) && (N <= 20))
+				{
+					std::cout << "Weird" << std::endl;
+				}
+				else if ((N % 2 == 0) && (20 <= N))
+				{
+					std::cout << "Not Weird" << std::endl;
+				}
+			}
+
+			
+	
+		system("pause");
+		return 0;
 }
