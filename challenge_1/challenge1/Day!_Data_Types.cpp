4edc44c0a9dacc8783835d89fc68b62300a22ebc
Jeanine Bray <jeaninebray@outlook.com>
2018-06-25 13:21:59 -0400

    New Day 1 challenge

---
+++ challenge_1/challenge1/Day!_Data_Types.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+int main()
+{
+	int i = 4;
+	double d = 4.0;
+	std::string s = "HackerRank ";
+
+	// Declare second integer, double, and String variables.
+	int b;
+	double c;
+	std::string e;
+
+	// Read and save an integer, double, and String to your variables.
+	// Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+	std::cin >> b;
+	std::cin >> c;
+	std::cin >> e;
+
+	// Print the sum of both integer variables on a new line.
+	std::cout << i + b;
+
+	// Print the sum of the double variables on a new line.
+	std::cout << d + c;
+
+	// Concatenate and print the String variables on a new line
+	// The 's' variable above should be printed first.
+	std::cout << s + e;
+
+	system("pause");
+	return 0;
+}
