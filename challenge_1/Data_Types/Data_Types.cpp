317cda74b3c3940043a8697781bd3c12cdb44062
Jeanine Bray <jeaninebray@outlook.com>
2018-06-25 14:57:53 -0400

    Update

--- challenge_1/Data_Types/Data_Types.cpp
+++ challenge_1/Data_Types/Data_Types.cpp
@@ -3,33 +3,36 @@
 #include <limits>
 #include <string>
 
+//using namespace std;
+
 int main()
 {
-	int i = 4;
-	double d = 4.0;
-	std::string s = "HackerRank ";
+		int i = 4;
+		double d = 4.0;
+		std::string s = "HackerRank ";
 
 	// Declare second integer, double, and String variables.
-	int b;
-	double c;
-	std::string e;
+		int b;
+		double c;
+		std::string e;
 
 	// Read and save an integer, double, and String to your variables.
 	// Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
-	std::cin >> b;
-	std::cin >> c;
-	std::cin >> e;
+		std::cin >> b;
+		std::cin >> c;
+		std::getline(std::cin >> std::ws, e);
 
 	// Print the sum of both integer variables on a new line.
-	std::cout << i + b;
+		std::cout << (i + b) << std::endl;
 
 	// Print the sum of the double variables on a new line.
-	std::cout << d + c;
+		std::cout.precision(1);
+		std::cout << std::fixed << (d + c) << std::endl;
 
 	// Concatenate and print the String variables on a new line
 	// The 's' variable above should be printed first.
-	std::cout << s + e;
+		std::cout << (s + e) << std::endl;
 
-	system("pause");
-	return 0;
+		system("pause");
+		return 0;
 }
